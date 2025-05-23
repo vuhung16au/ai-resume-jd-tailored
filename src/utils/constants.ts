ab840cf93d73c6631f7d0075a407eaaf72d9858a
Vu Hung <153976259+vuhung16au@users.noreply.github.com>
2025-05-23 10:39:44 +1000

    temporarily disabled pdf upload #20

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,12 +8,14 @@ export const MAX_FILE_SIZE_MB = 5; // 5MB
 
 // Accepted file types for resume and job descriptions
 export const ACCEPTED_FILE_TYPES = [
-  'application/pdf',
+  // TODO: fix #20 
+  // 'application/pdf',
   'application/msword',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   'text/plain'
 ];
 
 // File type descriptions
+// TODO: fix #20 
 export const ACCEPTED_FILE_EXTENSIONS = '.doc, .docx, .txt';
 // export const ACCEPTED_FILE_EXTENSIONS = '.pdf, .doc, .docx, .txt';
